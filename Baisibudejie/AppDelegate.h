8340df4ae3df1e018eb9e2a4fc54812bdd06f5dc
zhengya123 <919458797@qq.com>
2016-10-08 09:23:33 +0800

    Initial Commit

---
+++ Baisibudejie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baisibudejie
+//
+//  Created by dqong on 16/10/8.
+//  Copyright © 2016年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Baisibudejie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Baisibudejie
+//
+//  Created by dqong on 16/10/8.
+//  Copyright © 2016年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
